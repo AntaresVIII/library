6b3607420ad7098e23bd8903a321899e03a97dfd
Antares <enteringvalhalla@gmail.com>
2025-04-18 19:36:21 +0100

    Write most of logic. Unable to add book

--- script.js
+++ script.js
@@ -1,26 +1,30 @@
 const bookList = document.getElementById('book-list');
+const addBook = document.getElementById('add');
+const bookForm = document.getElementById('form');
+const createBook = document.getElementById
 
-const myLibrary = [
-    {
-        title: "The Hobbit",
-        author: "J.R.R Tolkien",
-        pages: "A lot",
-        read: undefined,
-    },
-    {
-        title: "The Mysterious Island",
-        author: "Jules Verne",
-        pages: "A lot",
-        read: undefined,
-    }
-];
+const myLibrary = [];
 
-function Book() {
-  // the constructor...
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
 }
 
 function addBookToLibrary() {
-  // take params, create a book then store it in the array
+    addBook.addEventListener('click', function(event) {
+        event.preventDefault();
+
+        const title = bookForm.title.value;
+        const author = bookForm.author.value;
+        const pages = bookForm.pages.value;
+        const read = bookForm.pages.value;
+
+        const createBook = new Book(title, author, pages, read);
+        myLibrary.push(Book);
+        console.log(myLibrary);
+    });
 }
 
 myLibrary.forEach(book => {
@@ -31,9 +35,22 @@ myLibrary.forEach(book => {
     <div class="book-title">${book.title}</div>
     <div class="book-author">by ${book.author}</div>
     <div class="book-pages">${book.pages}</div>
+    <div class="book-read">${book.read}</div>
     `;
 
     bookList.appendChild(bookCard);
+});
 
+addBook.addEventListener('click', () => {
+    if (bookForm.style.display === 'none' || bookForm.style.display === '') {
+        bookForm.style.display = 'block';
+        addBook.textContent = 'Cancel';
+    } else {
+        bookForm.style.display = 'none';
+        addBook.textContent = 'Add Book';
 
+        const inputs =  bookForm.querySelectorAll('input');
+        inputs.forEach(input => input.value = '');
+
+    }
 });
